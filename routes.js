95ba9c783e909ece51a7228c81d72e48a2b89da3
Andrei Stefanie <andrei.stefanie@gmail.com>
2019-06-13 18:05:16 +0300

    Disabled 2p auth endpoint

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('./src/controller');
 
-router.get('/2p-auth', controller.auth);
+// router.get('/2p-auth', controller.auth);
 
 router.put('/programs', controller.updatePrograms);
 router.get('/programs/:programId/promotions', controller.getProgramPromotions);
