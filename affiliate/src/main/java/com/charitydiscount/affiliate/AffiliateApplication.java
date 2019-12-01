72f1dfb10e52a29ce11580dd61307275f3b75b43
Andrei Stefanie <Andrei.Stefanie@msg.group>
2019-12-01 17:07:17 +0200

    Cors for affiliate service

--- affiliate/src/main/java/com/charitydiscount/affiliate/AffiliateApplication.java
+++ affiliate/src/main/java/com/charitydiscount/affiliate/AffiliateApplication.java
@@ -4,24 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 public class AffiliateApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(AffiliateApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(AffiliateApplication.class, args);
+    }
 }
 
 @Component
 class ProductServiceInterceptorAppConfig extends WebMvcConfigurationSupport {
-  @Autowired
-  private AffiliateInterceptor productServiceInterceptor;
+    @Autowired
+    private AffiliateInterceptor productServiceInterceptor;
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(productServiceInterceptor);
-  }
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://charitydiscount.ro",
+                "https://charitydiscount.github.io");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(productServiceInterceptor);
+    }
 }
 
--- affiliate/src/main/java/com/charitydiscount/affiliate/AffiliateInterceptor.java
+++ affiliate/src/main/java/com/charitydiscount/affiliate/AffiliateInterceptor.java
@@ -3,6 +3,8 @@ package com.charitydiscount.affiliate;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,24 +14,28 @@ import java.io.IOException;
 
 @Component
 public class AffiliateInterceptor implements HandlerInterceptor {
-  public AffiliateInterceptor() throws IOException {
-    FirebaseOptions options = new FirebaseOptions.Builder()
-        .setCredentials(GoogleCredentials.getApplicationDefault())
-        .setDatabaseUrl("https://charitydiscount.firebaseio.com/")
-        .build();
-    FirebaseApp.initializeApp(options);
-  }
+    public AffiliateInterceptor() throws IOException {
+        FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .setDatabaseUrl("https://charitydiscount.firebaseio.com/")
+            .build();
+        FirebaseApp.initializeApp(options);
+    }
 
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//    try {
-//      String token = request.getHeader(HttpHeaders.AUTHORIZATION).substring(6).trim();
-//      FirebaseAuth.getInstance().verifyIdToken(token);
-//    } catch (Exception e) {
-//      e.printStackTrace();
-//      throw new ForbiddenException();
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authorization == null || authorization.isEmpty()) {
+            throw new ForbiddenException();
+        }
+        try {
+            String token = authorization.substring(6).trim();
+            FirebaseAuth.getInstance().verifyIdToken(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ForbiddenException();
+        }
 
-    return true;
-  }
+        return true;
+    }
 }
