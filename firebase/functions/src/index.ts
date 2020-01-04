fb84ae12cc2e04cf630be3dc7a1ac0ee0f8203e8
Andrei Stefanie <andrei.stefanie@gmail.com>
2020-01-05 00:47:26 +0200

    Commented out index

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,43 +1,43 @@
-import * as functions from 'firebase-functions';
-import * as firestoreService from './services/firestore';
-import elasticService from './services/elastic';
-import * as controller from './controllers';
+// import * as functions from 'firebase-functions';
+// import * as firestoreService from './services/firestore';
+// import elasticService from './services/elastic';
+// import * as controller from './controllers';
 
-function getFunction(
-  timeoutSeconds: number = 300,
-  memory: typeof functions.VALID_MEMORY_OPTIONS[number] = functions
-    .VALID_MEMORY_OPTIONS[1],
-) {
-  const runtimeOpts = {
-    timeoutSeconds,
-    memory,
-  };
-  return functions.runWith(runtimeOpts).region('europe-west1');
-}
+// function getFunction(
+//   timeoutSeconds: number = 300,
+//   memory: typeof functions.VALID_MEMORY_OPTIONS[number] = functions
+//     .VALID_MEMORY_OPTIONS[1],
+// ) {
+//   const runtimeOpts = {
+//     timeoutSeconds,
+//     memory,
+//   };
+//   return functions.runWith(runtimeOpts).region('europe-west1');
+// }
 
-export const updatePrograms = getFunction()
-  .pubsub.schedule('every monday 06:00')
-  .timeZone('Europe/Bucharest')
-  .onRun(async (context: any) => {
-    const programs = await controller.getPrograms();
+// export const updatePrograms = getFunction()
+//   .pubsub.schedule('every monday 06:00')
+//   .timeZone('Europe/Bucharest')
+//   .onRun(async (context: any) => {
+//     const programs = await controller.getPrograms();
 
-    console.log(`Retrieved ${programs.length} programs`);
+//     console.log(`Retrieved ${programs.length} programs`);
 
-    const twoPCode = controller.getAffiliateCodes()[0].code;
-    const updatePromises = [
-      firestoreService.updatePrograms(programs),
-      firestoreService.updateMeta(twoPCode, programs),
-      elasticService.updateProgramsIndex(programs),
-    ];
+//     const twoPCode = controller.getAffiliateCodes()[0].code;
+//     const updatePromises = [
+//       firestoreService.updatePrograms(programs),
+//       firestoreService.updateMeta(twoPCode, programs),
+//       elasticService.updateProgramsIndex(programs),
+//     ];
 
-    return Promise.all(updatePromises).catch((e: Error) =>
-      console.log(e.message),
-    );
-  });
+//     return Promise.all(updatePromises).catch((e: Error) =>
+//       console.log(e.message),
+//     );
+//   });
 
-export const updatePendingCommissions = getFunction(540)
-  .pubsub.schedule('every 24 hours')
-  .timeZone('Europe/Bucharest')
-  .onRun((context: any) => {
-    return controller.updatePendingCommissions();
-  });
+// export const updatePendingCommissions = getFunction(540)
+//   .pubsub.schedule('every 24 hours')
+//   .timeZone('Europe/Bucharest')
+//   .onRun((context: any) => {
+//     return controller.updatePendingCommissions();
+//   });
